e0cfccbedc0ea1130895140acba906b4e151c454
Samuel De La Hoz <samueldlh.dev@gmail.com>
2025-10-06 22:18:32 -0500

    Initial implementations and dependencies

--- 03/target/debug/build/serde-59937e48836d45a3/build_script_build-59937e48836d45a3.d
+++
@@ -1,5 +0,0 @@
-/Users/kaonashi/study-zone/rust-by-example/03/target/debug/build/serde-59937e48836d45a3/build_script_build-59937e48836d45a3.d: /Users/kaonashi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs
-
-/Users/kaonashi/study-zone/rust-by-example/03/target/debug/build/serde-59937e48836d45a3/build_script_build-59937e48836d45a3: /Users/kaonashi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs
-
-/Users/kaonashi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs:
--- 03/target/debug/deps/todo_cli-587d0a1a620ab6be.d
+++
@@ -1,5 +0,0 @@
-/Users/kaonashi/study-zone/rust-by-example/03/target/debug/deps/todo_cli-587d0a1a620ab6be.d: src/main.rs
-
-/Users/kaonashi/study-zone/rust-by-example/03/target/debug/deps/libtodo_cli-587d0a1a620ab6be.rmeta: src/main.rs
-
-src/main.rs:
